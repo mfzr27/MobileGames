94ddb9fb8bed18336b3b46c6145130468f80e3f5
davidcorbetta <davidcorbetta@hotmail.com>
2017-08-09 20:43:35 -0300

    09/08

--- game1/www/js/index.js
+++ game1/www/js/index.js
@@ -5,18 +5,17 @@ var game = new Phaser.Game(
   '100%',
   Phaser.CANVAS,
   'Game Demo',
-  { preload: preload, create: create, update: update }
+  { preload: preload, create: create, update: update}
 );
 
 function preload() {
   game.load.spritesheet('player', 'img/player.png', 32, 48);
-  game.load.image('player', 'img/player.jpg');
   game.load.image('monster', 'img/monster.jpg');
   game.load.image('background', 'img/bg.jpg');
 
   game.load.audio('music', 'music/music1.mp3');
   game.load.audio('ping', 'sound/ping.mp3');
-}
+};
 
 function create() {
   var music = game.sound.play('music');
@@ -25,8 +24,9 @@ function create() {
 
   game.world.resize(2000, 2000);
 
-  game.add.sprite(0,0,'background');
+  game.add.sprite(0, 0, 'background');
 
+  //criar if para limitar a area de criacao para o tamanho da tela, utilizando game.width/game.height
   player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
   game.camera.follow(player);
   game.physics.enable(player, Phaser.Physics.ARCADE);
@@ -35,43 +35,59 @@ function create() {
   player.animations.add('walkRight', [8, 9, 10, 11]);
   player.animations.add('walkUp', [12, 13, 14, 15]);
 
-  monster = game.add.sprite(game.world.randomX, game.world.randomY, 'monster');  
+  monster = game.add.group();
+
+  for(var i = 0; i < 10; i++) {
+    var randomX = game.world.randomX;
+    var randomY = game.world.randomY;
+    var theMonster = monster.create(randomX, randomY, 'monster');
+    var styleName = { font: '25px Arial', fill: '#fff' };
+    var text = game.add.text(randomX, randomY, (i+1).toString(), styleName);
+    theMonster.theName = text;
+  }
   game.physics.enable(monster, Phaser.Physics.ARCADE);
 
   score = 0;
-  var style = { font: '20px Arial', fill: '#bddb28' };
-  txtScore = game.add.text(10, 10, score.toString(), style);
+  var style = {font:'25px Arial', fill: '#bddb28'};
+  txtScore = game.add.text(10, 100, score.toString(), style);
   txtScore.fixedToCamera = true;
+
   cursors = game.input.keyboard.createCursorKeys();
-}
+};
 
 function update() {
-  if (cursors.left.isDown) {
+  if(cursors.left.isDown) {
     player.animations.play('walkLeft', 5, true);
     player.x -= 2;
-  }
-  if (cursors.right.isDown) {
+  } else if(cursors.right.isDown) {
     player.animations.play('walkRight', 5, true);
     player.x += 2;
-  }
-  if (cursors.up.isDown) {
+  } else if(cursors.up.isDown) {
     player.animations.play('walkUp', 5, true);
     player.y -= 2;
-  }
-  if (cursors.down.isDown) {
+  } else if(cursors.down.isDown) {
     player.animations.play('walkDown', 5, true);
     player.y += 2;
-  }
-  game.physics.arcade.overlap(player, monster, monsterHitHandler);
-  if (!cursors.down.isDown && !cursors.up.isDown && !cursors.left.isDown && !cursors.right.isDown) {
+  } else {
     player.animations.stop();
   }
-}
+  game.physics.arcade.overlap(player, monster, monsterHitHandler);
+  if(!cursors.down.isDown && !cursors.up.isDown && !cursors.left.isDown && !cursors.right.isDown) {
+    player.animations.stop();
+  }
+
+};
 
 function monsterHitHandler(playerObject, monsterObject) {
   monsterObject.x = Math.random() * game.width;
   monsterObject.y = Math.random() * game.height;
+
   score++;
   txtScore.setText(score.toString());
-  var ping = game.sound.play('ping');
+
+  game.sound.play('ping');
+  monsterObject.theName.destroy();
+  monster.remove(monsterObject);
+
+  console.log(monsterObject.theName);
 }
